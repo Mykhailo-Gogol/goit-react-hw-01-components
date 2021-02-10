197b2462a13f23b4976f8375d3ac3922a72eb777
Mykhailo-Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2021-02-10 20:56:56 +0200

    Додає завдання 2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 import Profile from "./components/Profile";
 import profileData from "./user.json";
+import statisticsData from "./statistical-data.json";
+
+import Statistics from "./components/Statistics";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Profile profile={profileData} />
+        <Statistics title="Upload stats" stats={statisticsData} />
       </div>
     );
   }
